0c6b505ac7c6fe88d4d88c3e20830973ebe5ab46
Maxence Hammen <maxence.hammen@gmail.com>
2024-02-29 15:49:15 +0100

    Follow tutorial from chapters 6 to 12

--- app/ui/skeletons.tsx
+++ app/ui/skeletons.tsx
@@ -1,4 +1,6 @@
 // Loading animation
+import {lusitana} from "@/app/ui/fonts";
+
 const shimmer =
   'before:absolute before:inset-0 before:-translate-x-full before:animate-[shimmer_2s_infinite] before:bg-gradient-to-r before:from-transparent before:via-white/60 before:to-transparent';
 
